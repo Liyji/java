daec2ad4060e48dfa1d0d80039c6b31adc9965be
Liyji <agusbatur8@gmail.com>
2022-10-10 16:58:23 +0700

    java

---
+++ config.js
@@ -0,0 +1,57 @@
+/**
+   * Create By THE JO BOT
+   * Base by Dika Ardnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+
+global.ownername = 'Bo4n'
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+// Other
+global.owner = ['6285826226421','6287878293136','6287878293136']
+global.premium = ['6287878293136']
+global.packname = 'EllaAyg'
+global.author = 'Wan Black Negro'
+global.wlcm = []
+global.sessionName = 'session'
+global.prefa = ['','!','.','馃惁','馃悿','馃椏']
+global.sp = '猸�'
+global.mess = {
+    success: '鉁� Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/jo.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
